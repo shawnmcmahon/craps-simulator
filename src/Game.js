162a5467510717fa58b0387b91b1d063e568b408
Shawn McMahon <shawnmcmahon17@gmail.com>
2022-02-01 18:36:12 -0700

    Install Enyzme

--- src/Game.js
+++ src/Game.js
@@ -1,4 +1,14 @@
-function sum(a, b) {
-  return a + b;
+import React, { useState } from 'react'
+
+export default function Game() {
+  [diceRoll, setDiceRoll] = useState([1, 1]) 
+
+
+
+  return (
+    <div>
+      
+    </div>
+  )
 }
-module.exports = sum;
+
--- src/Game.test.js
+++ src/Game.test.js
@@ -1,5 +1,15 @@
-const sum = require('./Game');
+import Game from './Game';
+import React from 'react'; 
+import Enzyme, { mount } from 'enyzme'; 
+import Adapter from 'enzyme-adapter-react-16'; 
+Enzyme.configure({ adapter: new Adapter() })
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+  expect(Game.diceRoll).toBe([1,1]);
 });
+
+describe('Game Component', () => {
+  beforeEach(() => {
+
+  })
+})
