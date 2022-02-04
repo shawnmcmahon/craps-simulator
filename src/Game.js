9c2357fcda989a0e9e39de8ee63375221484a475
Shawn McMahon <shawnmcmahon17@gmail.com>
2022-02-03 21:12:55 -0700

    Add betting board hook to state

--- src/Game.js
+++ src/Game.js
@@ -4,6 +4,74 @@ export default function Game() {
   const [diceOne, setDiceOne] = useState(1); 
   const [diceTwo, setDiceTwo] = useState(1);
   const [point, setPoint] = useState(undefined);
+  const [board, setBoard] = useState({
+    four: {
+      buy: 0, 
+      lay: 0, 
+      come: 0, 
+      comeOdds: 0, 
+      dontCome: 0, 
+      dontComeOdds: 0, 
+    }, 
+    five: {
+      buy: 0, 
+      lay: 0, 
+      come: 0, 
+      comeOdds: 0, 
+      dontCome: 0, 
+      dontComeOdds: 0, 
+    },
+    six: {
+      buy: 0, 
+      lay: 0, 
+      come: 0, 
+      comeOdds: 0, 
+      dontCome: 0, 
+      dontComeOdds: 0, 
+    },
+    eight: {
+      buy: 0, 
+      lay: 0, 
+      come: 0, 
+      comeOdds: 0, 
+      dontCome: 0, 
+      dontComeOdds: 0, 
+    },
+    nine: {
+      buy: 0, 
+      lay: 0, 
+      come: 0, 
+      comeOdds: 0, 
+      dontCome: 0, 
+      dontComeOdds: 0, 
+    },
+    ten: {
+      buy: 0, 
+      lay: 0, 
+      come: 0, 
+      comeOdds: 0, 
+      dontCome: 0, 
+      dontComeOdds: 0, 
+    },
+    hardTwo: 0,
+    hardTen: 0,
+    hardSix: 0,
+    hardEight: 0, 
+    twoCraps: 0, 
+    threeCraps: 0, 
+    twelveCraps: 0, 
+    yoEleven: 0, 
+    sevenCraps: 0, 
+    anyCraps: 0, 
+    field: 0, 
+    passLine: 0, 
+    passLineOdds: 0, 
+    dontPass: 0, 
+    dontPassOdds: 0,
+    Come: 0, 
+    dontCome: 0, 
+
+  });
 
 
 
@@ -12,11 +80,8 @@ export default function Game() {
   }, [])
 
   const diceRoll = () => {
-    // const one =  Math.Floor(Math.Random * 6)
     setDiceOne(Math.floor(Math.random() * 6) + 1);
-
     setDiceTwo(Math.floor(Math.random() * 6) + 1); 
-
     console.log(diceOne, diceTwo)
   };
 
